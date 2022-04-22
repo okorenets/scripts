4fd30d55a95ecccbf3510ffc43b61821461c0974
Oleg <okorenets@lachestry.com>
2022-04-22 12:02:14 +0300

    Update find and sort copy

--- module2.sh
+++ module2.sh
@@ -3,66 +3,67 @@
 DATE=$(date +"%Y-%m-%d_%H-%M")
 SRC=~/test2/module2
 DST=~/backups/${DATE}
+FIND_TXT=`find ${SRC} -name "*.txt"`
+FIND_JPG=`find ${SRC} -name "*.jpg"`
+FIND_MP3=`find ${SRC} -name "*.mp3"`
 JPG=${DST}/pic/*.jpg
 
 if
     [ ! -d "${DST}" ];then 
     mkdir -p ${DST}
-    echo 'mkdir for backups'
+    echo "Создание директории ${DST}" >> ~/backups/log_${DATE}.txt
 else 
     rm -rf ${DST}/*
-    echo 'backups already exist! it`s cleared'
+    echo 'backups already exist! it`s cleared' >> ~/backups/log_${DATE}.txt
 fi
  
 if [ -d "${DST}" ]; then
     mkdir -p ${DST}/txt ${DST}/pic ${DST}/mus ${DST}/compressed_img
-    echo 'mkdir /txt /pic /mus /compressed_img'
+    echo -e "Создание поддиректорий \n${DST}/txt \n${DST}/pic \n${DST}/mus \n${DST}/compressed_img" >> ~/backups/log_${DATE}.txt
 fi
 
-    echo 'Start copying data from SRC to DST with sort'
+    echo "Start copying data from ${SRC} to ${DST} with sort" >> ~/backups/log_${DATE}.txt
 
-if
-    find ${SRC} -name "*.txt" -exec cp -r {} ${DST}/txt \;
-then
-    echo 'Succes copy *.txt files'
+if [[ -z ${FIND_TXT} ]]; then
+    echo ".txt files not found" >> ~/backups/log_${DATE}.txt
 else
-    echo "txt files not found"
+    cp -R ${FIND_TXT} ${DST}/txt
+    echo 'Succes copy *.txt files' >> ~/backups/log_${DATE}.txt
 fi
 
-if
-    tar -zcpf ${DST}/txt/txt.tar ${DST}/txt
-    echo 'Tar DST/txt'
-then
-    rm -rf ${DST}/txt/*.txt
-    echo 'Rm txt files from DST/txt'
+# if
+#     tar -zcpf ${DST}/txt/txt.tar ${DST}/txt
+#     echo 'Tar DST/txt'
+# then
+#     rm -rf ${DST}/txt/*.txt
+#     echo 'Rm txt files from DST/txt'
+# else
+#     echo 'fail tar'
+# fi
+
+if [[ -z ${FIND_JPG} ]]; then
+    echo ".jpg files not found" >> ~/backups/log_${DATE}.txt
 else
-    echo 'fail tar'
+    cp -R ${FIND_JPG} ${DST}/pic
+    echo 'Succes copy *.jpg files' >> ~/backups/log_${DATE}.txt
 fi
 
-if
-    find ${SRC} -name "*.jpg" -exec cp -R {} ${DST}/pic \;
-then
-    echo 'Succes copy *.jpg files'
+if [[ -z ${FIND_MP3} ]]; then
+    echo ".mp3 files not found" >> ~/backups/log_${DATE}.txt
 else
-    echo "jpg files not found"
+    cp -R ${FIND_MP3} ${DST}/mus
+    echo 'Succes copy *.mp3 files' >> ~/backups/log_${DATE}.txt
 fi
 
-if
-    find ${SRC} -name "*.mp3" -exec cp -R {} ${DST}/mus \;
-then
-    echo 'Succes copy *.mp3 files'
-else
-    echo "mp3 files not found"
-fi
-
-    echo 'All data has been copied' ${DATE} >> ~/backups/log.txt
+    echo 'All data has been copied' ${DATE} >> ~/backups/log_${DATE}.txt
 
 for file in ${JPG}; do
     mogrify "$file" -quality 80% -path ${DST}/compressed_img "$file"
 done;
-
+    cat ~/backups/log_${DATE}.txt
+    
     check_ini () {
-        CHK=`cat backup.ini | grep to_external_server=1`
+        CHK=`cat backup.ini | grep to_external_server=1 | grep -v grep`
     }
     # servers () {
     #     SRV=`cat backup.ini | `
